af705f7f28b6e93866526c62de8d4ff7a19e86aa
sadra <sadrayan@gmail.com>
2016-10-07 22:36:30 -0400

    index wikipedia

---
+++ src/main/java/org/experiment/wikipedia/IndexWikipedia.java
@@ -0,0 +1,105 @@
+package org.experiment.wikipedia;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.benchmark.byTask.feeds.DocMaker;
+import org.apache.lucene.benchmark.byTask.feeds.EnwikiContentSource;
+import org.apache.lucene.benchmark.byTask.utils.Config;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+
+
+/**
+ * Created by sonic on 07/10/16.
+ */
+public class IndexWikipedia {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length <= 1) {
+            printUsage();
+            return;
+        }
+        File wikipediafile = new File(args[0]);
+        if (!wikipediafile.exists()) {
+            System.out.println("Can't find " + wikipediafile.getAbsolutePath());
+            return;
+        }
+        if (!wikipediafile.canRead()) {
+            System.out.println("Can't read " + wikipediafile.getAbsolutePath());
+            return;
+        }
+        File outputDir = new File(args[1]);
+        if (!outputDir.exists()) {
+            if (!outputDir.mkdirs()) {
+                System.out.println("couldn't create " + outputDir.getAbsolutePath());
+                return;
+            }
+        }
+        if (!outputDir.isDirectory()) {
+            System.out.println(outputDir.getAbsolutePath() + " is not a directory!");
+            return;
+        }
+        if (!wikipediafile.canWrite()) {
+            System.out.println("Can't write to " + outputDir.getAbsolutePath());
+            return;
+        }
+
+        // we should be "ok" now
+
+        FSDirectory dir = FSDirectory.open(Paths.get(args[1]));
+
+        StandardAnalyzer analyzer = new StandardAnalyzer();// default stop words
+
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);// overwrites if needed
+
+        IndexWriter indexWriter = new IndexWriter(dir, config);
+
+        DocMaker docMaker = new DocMaker();
+
+        Properties properties = new Properties();
+        properties.setProperty("content.source.forever", "false");
+        properties.setProperty("docs.file", wikipediafile.getAbsolutePath());
+        properties.setProperty("keep.image.only.docs", "false");
+        Config c = new Config(properties);
+        EnwikiContentSource source = new EnwikiContentSource();
+        source.setConfig(c);
+        source.resetInputs();// though this does not seem needed, it is
+        // (gets the file opened?)
+        docMaker.setConfig(c, source);
+        int count = 0;
+        System.out.println("Starting Indexing of Wikipedia dump " + wikipediafile.getAbsolutePath());
+        long start = System.currentTimeMillis();
+        Document doc;
+        try {
+            while ((doc = docMaker.makeDocument()) != null) {
+                indexWriter.addDocument(doc);
+                ++count;
+                if (count % 1000 == 0) {
+                    System.out.println("Indexed " + count + " documents in "
+                            + (System.currentTimeMillis() - start) + " ms");
+                }
+            }
+        } catch (org.apache.lucene.benchmark.byTask.feeds.NoMoreDataException nmd) {
+            nmd.printStackTrace();
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println("Indexing " + count + " documents took " + (finish - start) + " ms");
+        System.out.println("Total data processed: " + source.getTotalBytesCount() + " bytes");
+        System.out.println("Index should be located at " + dir.getDirectory());
+        docMaker.close();
+        indexWriter.close();
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: java -cp <...> me.lemire.lucene.IndexDump somewikipediadump.xml.gz outputdir");
+    }
+
+}
